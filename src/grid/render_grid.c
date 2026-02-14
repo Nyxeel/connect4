91c853cd5417b0c2dd2c807a177a437e92bf87a7
Nyxeel <nyxeel@protonmail.com>
2026-02-14 14:28:06 +0100

    STARTED: to implement input line to drop pawns

--- src/grid/render_grid.c
+++ src/grid/render_grid.c
@@ -23,10 +23,11 @@ int	color_cell(t_data *data, Cell *cell, int x, int y)
 {
 	char map_num = data->map[y][x];
 	int start_x = 1 + x * (cell->w + 1);
-	int start_y = 1 + y * (cell->h + 1);
+	int start_y = 1 + (y + 1) * (cell->h + 1);
 
 	short color = get_color(map_num);
-	for (int row = 0; row < cell->h; row++){
+	for (int row = 0; row < cell->h; row++)
+	{
 		move(start_y + row, start_x);
 		if( start_y < data->terminal_max_y)
 			chgat(cell->w, A_NORMAL, color, NULL);
@@ -59,16 +60,16 @@ static void compute_cell_size(t_data *data, Cell *cell)
 {
 
     int usable_w = data->terminal_max_x - (data->columns + 1);		//Calculate the usable width of the window for the grid.
-    int usable_h = data->terminal_max_y - (data->rows + 1);		//same for height
+    int usable_h = data->terminal_max_y - (data->rows + 2);		//same for height
 
-    if (usable_w < data->columns || usable_h < data->rows) // Safety check: if the usable area is smaller than the grid
+    if (usable_w < data->columns || usable_h < data->rows + 1) // Safety check: if the usable area is smaller than the grid
     {
         cell->w = 2;
         cell->h = 1;
         return;
     }
 	cell->w = usable_w / data->columns;					//calc usable cell witdh and height by dividing evenly
-	cell->h = usable_h / data->rows;
+	cell->h = usable_h / (data->rows + 1);
 	if (cell->w < 3)										//enforce minimum cellsize for cell_w and cell_h
 	    cell->w = 2;
 	if (cell->h < 2)
@@ -85,7 +86,7 @@ int render_grid(t_data *data, Cell *cell)
     clear();
 
 	int need_w = data->columns * (cell->w + 1) + 1;
-	int need_h = data->rows * (cell->h + 1) + 1;
+	int need_h = (data->rows + 1) * (cell->h + 1) + 1;
 	if (need_w > data->terminal_max_x || need_h > data->terminal_max_y)
 	{
 		printw("Terminal too small! Minimum size: [%d, %d]\n", need_w, need_h);
@@ -94,7 +95,7 @@ int render_grid(t_data *data, Cell *cell)
 	}
 
     int y = 0;
-    for (int row = 0; row < data->rows; row++)
+    for (int row = 1; row < data->rows + 1; row++)
     {
         int x = 0;
         for (int col = 0; col < data->columns; col++)
